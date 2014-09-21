1cd8f1c8a372c75ac7492f3d6fc3c53df8962f1f
KT8 <kartongw@gmail.com>
2014-09-22 02:35:53 +0800

    small change to ui.R to add documentation link

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Effect of Sample Size on Sample mean and Sample standard deviation"),
   sidebarPanel(
+    p(em("Documentation:",a("StatsEducator",href="index.html"))),
     numericInput('lamda', 'Lamda of Poisson Distribution', 2, min = 0, max = 50, step = 1),
     numericInput('numsim', 'Number of simulations', 1000, min = 0, max = 3000, step = 10),
     numericInput('n1', 'Sample Size 1', 10, min = 3, max = 2000, step = 1),
@@ -17,6 +18,8 @@ shinyUI(pageWithSidebar(
     plotOutput('TwoHist')
     
   )
+  
+  
 ))
 
 
